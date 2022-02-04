2eea408f4b159f6309d5a88e9d04d0cc6744a489
olejajew <oleg@nwcode.ru>
2022-02-04 20:09:49 +0300

    bug fix with images

--- analytics_module/src/main/kotlin/com/hdhgbot/analyticsbot/messaging/helpers/SendMessageHelper.kt
+++ analytics_module/src/main/kotlin/com/hdhgbot/analyticsbot/messaging/helpers/SendMessageHelper.kt
@@ -7,6 +7,7 @@ import com.hdhgbot.analyticsbot.messaging.data.UrlButtonModel
 import com.hdhgbot.analyticsbot.utils.ListUtils
 import com.hdhgbot.botlibrary.BotButton
 import com.hdhgbot.botlibrary.models.ImageInputStream
+import java.io.ByteArrayInputStream
 
 class SendMessageHelper(private val analyticsModule: AnalyticsModule) {
 
@@ -33,22 +34,23 @@ class SendMessageHelper(private val analyticsModule: AnalyticsModule) {
     private fun sendMessageWithImageAndButtons(mailingModel: MailingModel) {
         val chatMailingProvider = analyticsModule.getDatabaseHelper().chatMailingTableProvider
         val imageName = ListUtils.convertStringToList(mailingModel.images)[0]
-        val image = analyticsModule.getFilesProvider().getImage(imageName)
-        println("HERE = " + image)
         val buttonsStringList = ListUtils.convertStringToList(mailingModel.buttons)
         val buttons = buttonsStringList.map {
             val urlButtonModel = UrlButtonModel(it)
             BotButton(urlButtonModel.buttonText, "", urlButtonModel.buttonUrl)
         }
         val listButtons = arrayListOf(buttons)
-        val imageInputStream = ImageInputStream(
-            image,
-            imageName
-        )
+        val file = analyticsModule.getFilesProvider().getImage(imageName)
+        val byteArray = file.readBytes()
         analyticsModule.getUsersProvider().getAliveUsers().forEach {
             val message =
                 analyticsModule.getChatBot()
-                    .sendMessageWithImageInputStream(imageInputStream, it, mailingModel.message, listButtons)
+                    .sendMessageWithImageInputStream(
+                        ImageInputStream(
+                            ByteArrayInputStream(byteArray.clone()),
+                            imageName
+                        ), it, mailingModel.message, listButtons
+                    )
 
             if (message != null) {
                 chatMailingProvider.saveMailingMessage(
@@ -66,9 +68,10 @@ class SendMessageHelper(private val analyticsModule: AnalyticsModule) {
         val chatMailingProvider = analyticsModule.getDatabaseHelper().chatMailingTableProvider
         val imageName = ListUtils.convertStringToList(mailingModel.images)[0]
         val file = analyticsModule.getFilesProvider().getImage(imageName)
+        val byteArray = file.readBytes()
         analyticsModule.getUsersProvider().getAliveUsers().forEach {
             val imageInputStream = ImageInputStream(
-                file,
+                ByteArrayInputStream(byteArray.clone()),
                 imageName
             )
             val message = analyticsModule.getChatBot().sendMessageWithImageInputStream(
